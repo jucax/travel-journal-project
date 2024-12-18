75be1eed398210e306070190fa41b34dd8f5af05
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-12-18 12:43:53 -0600

    Created Navbar and Trip components

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="img/world-icon.png" className="navbar--logo"/>
+            <p className="navbar--text"> my travel journal.</p>
+        </nav>
+    )
+}
---
+++ src/components/Trip.js
@@ -0,0 +1,17 @@
+export default function Trip() {
+    return (
+        <div className="trip--container">
+            <img src="img/japan-trip.png" className="trip--picture"></img>
+            <div>
+                <div className="trip--location-container">
+                    <img src="img/location-icon.png" className="trip-location-icon"></img>
+                    <h3>JAPAN</h3>
+                    <a>View on Google Maps</a>
+                </div>
+                <h1>Mount Fuji</h1>
+                <h2>12 Jan, 2023 - 24 Jan, 2023</h2>
+                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            </div>
+        </div>
+    )
+}
